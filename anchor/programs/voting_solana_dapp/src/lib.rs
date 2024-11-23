57198d7f20c9d507b4183ee3a1cfc87b64057a3e
to4to <justjustifying@outlook.com>
2024-11-23 12:16:30 +0530

    refactor: rename initilize_poll to initialize_poll and update related structures

--- anchor/programs/voting_solana_dapp/src/lib.rs
+++ anchor/programs/voting_solana_dapp/src/lib.rs
@@ -9,7 +9,26 @@ pub mod voting {
 
     use super::*;
 
-    pub fn initilize_poll(ctx: Context<InitilizePoll>, poll_id: u64) -> ProgramResult {
+    pub fn initialize_poll(ctx: Context<InitializePoll>, poll_id: u64) -> Result<()> {
         Ok(())
     }
+}
+
+#[derive(Accounts)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account()]
+    pub poll: Account<'info, Poll>,
+}
+
+#[account]
+
+pub struct Poll {
+    pub poll_id: u64,
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
 }
